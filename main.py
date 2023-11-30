8cfb216f2f15395eb28e5ac659b06356f4d5c9ef
Julian Quispe <julianmql@hotmail.com>
2023-11-30 03:01:57 -0500

    feat: start_date filter added

--- main.py
+++ main.py
@@ -11,8 +11,23 @@ DEFAULT_LAWS_FOLDER = f"{DEFAULT_DATA_FOLDER}/laws"
 DEFAULT_SAVED_VERSION_FILEPATH = f"{DEFAULT_DATA_FOLDER}/saved_version.json"
 
 
-async def download_laws():
-    urls = await get_legal_laws_urls(DEFAULT_MONTFERRET_WORKER_URL)
+async def download_laws(
+        day_from: int,
+        month_from: int,
+        year_from: int,
+        day_to: int,
+        month_to: int,
+        year_to: int
+):
+    params = {
+        "day_from": day_from,
+        "month_from": month_from - 1,
+        "year_from": year_from,
+        "day_to": day_to,
+        "month_to": month_to - 1,
+        "year_to": year_to
+    }
+    urls = await get_legal_laws_urls(DEFAULT_MONTFERRET_WORKER_URL, params= params)
     await download_laws_to_disk(urls, folder_path=DEFAULT_LAWS_FOLDER)
 
 
@@ -32,7 +47,7 @@ async def load_laws_as_index(saved_version_filepath: str) -> BaseGPTIndex:
 async def main():
     load_dotenv()
 
-    await download_laws()
+    await download_laws(1,11,2023,20,11,2023)
     # laws_index = await load_laws_as_index(DEFAULT_SAVED_VERSION_FILEPATH)
     # res = laws_index.query(
     #     "Resume los cambios más importantes en el sistema de justicia penal",
